7e54b0803d592698918aeedf7384e8c63d6542d2
rintaro <rintaro.onaka@d2c.co.jp>
2020-06-25 10:59:17 +0900

    Add: app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template
+from flask import request
+
 from flask import make_response
 @app.route("/cookie")
 def sample_cookie():
